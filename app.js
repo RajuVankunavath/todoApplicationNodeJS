607ce9fad26262ac689a1f6bf3b1e852f33cc487
Rajuvankunavath <vankunavathraju8978@gmail.com>
2025-01-16 17:47:58 +0000

    todonodejs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
 const format = require('date-fns/format')
 const isValid = require('date-fns/isValid')
 const toDate = require('date-fns/toDate')
